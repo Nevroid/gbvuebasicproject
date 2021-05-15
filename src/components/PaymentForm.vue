88a0da7965f8dfade17bb23b092a487370162c71
KirTim <kirtim1986@gmail.com>
2021-05-15 17:57:04 +0300

    started new app. hw done
    components added

---
+++ src/components/PaymentForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :class="[$style.form]">
+      <input :class="[$style.form__field]" type="date" placeholder="Date" v-model="date">
+      <select :class="[$style.form__field]" v-model="category">
+        <option value="Education" >Education</option>
+        <option value="Food">Food</option>
+        <option value="HealthCare">HealthCare</option>
+      </select>
+      <input :class="[$style.form__field]" type="number" placeholder="Spended" v-model.number="spended">
+      <button :class="[$style.form__field, $style.saveBtn]" @click="save">Save</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      date: '',
+      category: '',
+      spended: 0
+    }
+  },
+  methods: {
+    save () {
+      const { date, category, spended } = this
+      this.$emit('paymentFormDataEmit', { date, category, spended })
+      this.$emit('recountPages')
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+  .form {
+    position: absolute;
+    height: 180px;
+    width: 300px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+  .form__field {
+    height: 40px;
+    border-radius: 16px;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+  .saveBtn {
+    color: white;
+    background: #22a698;
+  }
+</style>
---
+++ src/components/PaymentList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <table :class="[$style.table]">
+        <tr>
+            <th :class="[$style.th]" v-for="header in headers" :key="header">{{ header }}</th>
+        </tr>
+        <tr :class="[$style.tr]" v-for="item in items" :key="item.index">
+            <td>{{ item.index }}</td>
+            <td>{{ item.date }}</td>
+            <td>{{ item.category }}</td>
+            <td>{{ item.spended }}</td>
+        </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      headers: ['#', 'Date', 'Category', 'Spended']
+    }
+  },
+  props: {
+    items: Array
+  }
+}
+</script>
+
+<style lang="scss" module>
+    .table {
+      background: lightblue;
+      font-size: 18px;
+      text-align: center;
+    }
+    .th {
+      width: 100px;
+      background: lightcoral;
+    }
+    .tr {
+      border: 1px solid black;
+    }
+</style>
